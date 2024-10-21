96f6f04b79ad29d38b1d8547bf8b7f600e510fed
Marc Schröder <marc.schroeder@tu-dortmund.de>
2024-10-21 10:52:30 +0200

    Beginning

---
+++ V18/vXXX/plot.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+import pandas as pd
+import scipy.constants as const
+import math
+
